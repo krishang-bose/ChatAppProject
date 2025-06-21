c021e6b2dd615748c086be3f93b6af46f21b1266
Krishang <144909920+krishang-bose@users.noreply.github.com>
2025-06-22 01:14:31 +0530

    working till this point

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/", // Update to backend server's URL
+    baseURL: "http://localhost:5001/api", 
     withCredentials: true,
 });
